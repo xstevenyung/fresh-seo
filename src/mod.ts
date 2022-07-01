ebc8d11474572646e406114ce1c544f6e59b69a2
Steven Yung <stevenyung@fastmail.com>
2022-07-01 23:08:10 +0200

    add mandatory url

--- src/mod.ts
+++ src/mod.ts
@@ -4,11 +4,16 @@ export const DEFAULT_OPTIONS = {
   staticDir: "./static",
 };
 
-export async function createSitemap(manifest: any, options = DEFAULT_OPTIONS) {
-  const outPath = join(options.staticDir, "sitemap.xml");
+export type Options = {
+  staticDir: string;
+  url: string;
+};
+
+export async function createSitemap(url: string, manifest: any) {
+  const outPath = join(DEFAULT_OPTIONS.staticDir, "sitemap.xml");
 
   try {
-    const sitemap = generateSitemap(manifest);
+    const sitemap = generateSitemap(url, manifest);
 
     await ensureFile(outPath);
 
@@ -19,64 +24,52 @@ export async function createSitemap(manifest: any, options = DEFAULT_OPTIONS) {
   }
 }
 
-export function generateSitemap(manifest: any): string {
-  const baseURL = Deno.env.get("APP_URL");
-
-  if (!baseURL) {
-    throw new Error(
-      "We are missing the APP_URL environment variable to generate the sitemap.",
-    );
-  }
-
-  return `
-    <?xml version="1.0" encoding="UTF-8"?>
+export function generateSitemap(url: string, manifest: any): string {
+  return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-      ${
-    Object.entries(manifest.routes)
-      .filter(([path]) => {
-        const isRootRoute = "./routes" === dirname(path);
-        const file = basename(path);
-        const fileName = file.replace(extname(file), "");
+      ${Object.entries(manifest.routes)
+        .filter(([path]) => {
+          const isRootRoute = "./routes" === dirname(path);
+          const file = basename(path);
+          const fileName = file.replace(extname(file), "");
 
-        if (isRootRoute && fileName === "_404") {
-          return false;
-        }
+          if (isRootRoute && fileName === "_404") {
+            return false;
+          }
 
-        return true;
-      })
-      .map(([path, route]) => {
-        const fileName = basename(path).replace(extname(path), "");
-        const isDynamic = !!fileName.match(/^\[.+\]$/)?.length;
+          return true;
+        })
+        .map(([path, route]) => {
+          const fileName = basename(path).replace(extname(path), "");
+          const isDynamic = !!fileName.match(/^\[.+\]$/)?.length;
 
-        path = path
-          .replace(extname(path), "")
-          .replace("./routes", baseURL)
-          .replace("index", ""); // We remove index as it's consider a "/" in Fresh
+          path = path
+            .replace(extname(path), "")
+            .replace("./routes", url)
+            .replace("index", ""); // We remove index as it's consider a "/" in Fresh
 
-        if (isDynamic) {
-          /** @ts-ignore */
-          const routes = route.sitemap ? route.sitemap() : [];
+          if (isDynamic) {
+            /** @ts-ignore */
+            const routes = route.sitemap ? route.sitemap() : [];
 
-          /** @ts-ignore */
-          return routes.map((route) => {
-            return `<url>
+            /** @ts-ignore */
+            return routes.map((route) => {
+              return `<url>
             <loc>${path.replace(fileName, route)}</loc>
             <lastmod>${day().format("YYYY-MM-DD")}</lastmod>
             <changefreq>daily</changefreq>
             <priority>0.8</priority>
           </url>`;
-          });
-        }
+            });
+          }
 
-        return `<url>
+          return `<url>
           <loc>${path}</loc>
           <lastmod>${day().format("YYYY-MM-DD")}</lastmod>
           <changefreq>daily</changefreq>
           <priority>0.8</priority>
         </url>`;
-      })
-      .join("\n")
-  }
-    </urlset>
-  `;
+        })
+        .join("\n")}
+    </urlset>`;
 }
