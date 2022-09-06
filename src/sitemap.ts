5e32b1611da631d8fba5f28ce7224bd8ecbf909d
Angel Mario <savin@angelmario.eu>
2022-09-06 17:28:52 +0300

    Added remove method (#8)

--- src/sitemap.ts
+++ src/sitemap.ts
@@ -3,6 +3,7 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 /// <reference lib="deno.unstable" />
+
 import day from "dayjs";
 import { basename, extname } from "path";
 import { Manifest } from "fresh/server.ts";
@@ -11,61 +12,62 @@ export class SitemapContext {
   #url: string;
   #manifest: Manifest;
   #globalIgnore = ["sitemap.xml"];
-  #additionalRoutes: Array<string> = [];
+  #routes: Array<string> = [];
 
   constructor(url: string, manifest: Manifest) {
     this.#url = url;
     this.#manifest = manifest;
+    this.#routes = Object.entries(this.#manifest.routes)
+      .filter(([path]) => {
+        // const isRootRoute = "./routes" === dirname(path);
+        const file = basename(path);
+        const fileName = file.replace(extname(file), "");
+        const isDynamic = !!fileName.match(/^\[.+\]$/)?.length;
+
+        if (
+          isDynamic || fileName.startsWith("_") ||
+          this.#globalIgnore.includes(fileName)
+        ) {
+          return false;
+        }
+
+        return true;
+      })
+      .map(([path, route]) => {
+        return path
+          .replace(extname(path), "")
+          .replace("./routes", "")
+          .replace("index", ""); // We remove index as it's consider a "/" in Fresh
+      })
   }
 
   get routes() {
-    return [
-      ...Object.entries(this.#manifest.routes)
-        .filter(([path]) => {
-          // const isRootRoute = "./routes" === dirname(path);
-          const file = basename(path);
-          const fileName = file.replace(extname(file), "");
-          const isDynamic = !!fileName.match(/^\[.+\]$/)?.length;
-
-          if (
-            isDynamic || fileName.startsWith("_") ||
-            this.#globalIgnore.includes(fileName)
-          ) {
-            return false;
-          }
-
-          return true;
-        })
-        .map(([path, route]) => {
-          return path
-            .replace(extname(path), "")
-            .replace("./routes", "")
-            .replace("index", ""); // We remove index as it's consider a "/" in Fresh
-        }),
-
-      ...this.#additionalRoutes,
-    ];
+    return this.#routes;
   }
 
   add(route: string) {
-    this.#additionalRoutes.push(route);
+    this.#routes.push(route);
+    return this;
+  }
+
+  remove(route: string) {
+    this.#routes = this.#routes.filter(r => r !== route);
     return this;
   }
 
   generate() {
     return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml">
-      ${
-      this.routes.map((route) => {
-        return `<url>
+      ${this.routes.map((route) => {
+      return `<url>
           <loc>${this.#url}${route}</loc>
           <lastmod>${day().format("YYYY-MM-DD")}</lastmod>
           <changefreq>daily</changefreq>
           <priority>0.8</priority>
         </url>`;
-      })
+    })
         .join("\n")
-    }
+      }
     </urlset>`;
   }
 
