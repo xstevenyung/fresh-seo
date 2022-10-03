c643ecaf9e4b3ffcc6ced50f26e120136d1601eb
sinyo-matu <sinyo_matu@icloud.com>
2022-10-03 20:29:25 +0900

    Adding generating robots.txt function to init.ts.
    init.ts will recept a user definition url for sitemap.xlm.ts and robots.txt generation

--- init.ts
+++ init.ts
@@ -1,24 +1,48 @@
 import { ensureFile, join, resolve } from "./src/deps.ts";
 
-const routesDirectory = resolve("./routes");
+export async function createSitemap(url: string | null) {
+  const routesDirectory = resolve("./routes");
 
-const destination = join(routesDirectory, "sitemap.xml.ts");
+  const destination = join(routesDirectory, "sitemap.xml.ts");
 
-await ensureFile(destination);
+  await ensureFile(destination);
 
-const stub = `import { Handlers } from "$fresh/server.ts";
+  const stub = `import { Handlers } from "$fresh/server.ts";
 import manifest from "../fresh.gen.ts";
 import { SitemapContext } from "https://deno.land/x/fresh_seo@0.1.1/mod.ts";
-
 export const handler: Handlers = {
   GET(req, ctx) {
-    const sitemap = new SitemapContext("http://example.com", manifest);
-
+    const sitemap = new SitemapContext("${
+    url ?? "http://exmaple.com"
+  }", manifest);
     // You can add additional page here
-
     return sitemap.render();
   },
 };
 `;
 
-await Deno.writeTextFile(destination, stub);
+  return Deno.writeTextFile(destination, stub);
+}
+
+export async function createRobotTxt(url: string | null) {
+  const directory = resolve("./static");
+
+  const destination = join(directory, "robots.txt");
+
+  await ensureFile(destination);
+
+  const stub = `User-agent: *
+Allow: /
+Disallow: /api/*
+Sitemap: ${url ?? "http://example.com"}/sitemap.xml
+`;
+
+  return Deno.writeTextFile(destination, stub);
+}
+
+const url = prompt(
+  "Please input your site's url (skip by enter):",
+  "http://example.com",
+);
+await createSitemap(url);
+await createRobotTxt(url);
