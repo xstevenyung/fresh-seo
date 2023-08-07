55928c9da4fe8027a2751c030401b5f953e2302a
Savin Angel-Mario <savin@angelmario.eu>
2023-08-07 14:48:09 +0300

    Updated CI

--- src/deps.ts
+++ src/deps.ts
@@ -3,12 +3,12 @@ export {
   extname,
   join,
   resolve,
-} from "https://deno.land/std@0.153.0/path/mod.ts";
+} from "https://deno.land/std@0.197.0/path/mod.ts";
 export {
   assert,
   assertStringIncludes,
   assertThrows,
-} from "https://deno.land/std@0.145.0/testing/asserts.ts";
-export { FakeTime } from "https://deno.land/std@0.145.0/testing/time.ts";
-export { ensureFile } from "https://deno.land/std@0.128.0/fs/mod.ts";
+} from "https://deno.land/std@0.197.0/testing/asserts.ts";
+export { FakeTime } from "https://deno.land/std@0.197.0/testing/time.ts";
+export { ensureFile } from "https://deno.land/std@0.197.0/fs/mod.ts";
 export { filterFiles } from "https://deno.land/x/glob_filter@1.0.0/mod.ts";
